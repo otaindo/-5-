d004000f6ec2342fc1ef3fa6eaaa55ce00b95476
otaindo <km127089@gmail.com>
2025-09-18 09:41:53 +0500

    Update Задание 1.go

--- Задание 1.go
+++ Задание 1.go
@@ -1,1 +1,23 @@
+import (
+ "fmt"
+ "time"
+)
 
+func main() {
+
+ resultCh := make(chan string)
+
+
+ go func() {
+  time.Sleep(3 * time.Second) 
+  resultCh <- "Результат запроса"
+ }()
+
+
+ select {
+ case result := <-resultCh:
+  fmt.Println("Получен результат:", result)
+ case <-time.After(2 * time.Second):
+  fmt.Println("Таймаут: сервер не ответил за 2 секунды")
+ }
+}
